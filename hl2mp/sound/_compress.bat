480e84f9610ac7d17928d3df7868ef34ebb98668
Nevswrld <nevanlombardo@gmail.com>
2025-07-16 11:12:02 -0400

    added floodgate

---
+++ hl2mp/sound/_compress.bat
@@ -0,0 +1,42 @@
+@echo off & setlocal enabledelayedexpansion
+for %%v in ("*.nav") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.bsp") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.vtx") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.mdl") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.phy") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.vvd") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.vmt") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.vmf") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.vtf") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+for %%v in ("*.wav") do (
+  7za a "%%v.bz2" "%%v"
+  del "%%v"
+)
+pause
